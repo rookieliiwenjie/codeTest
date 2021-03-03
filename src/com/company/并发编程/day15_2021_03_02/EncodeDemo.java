492aadc168d95d9dbd74a629fd0ac9a51f7ef196
liwenjie19 <1va-idrdtmg38e@dingtalk.com>
2021-03-03 11:51:23 +0800

    feat:等待超时模式

--- src/com/company/并发编程/day15_2021_03_02/EncodeDemo.java
+++ src/com/company/并发编程/day15_2021_03_02/EncodeDemo.java
@@ -1,5 +1,6 @@
 package com.company.并发编程.day15_2021_03_02;
 
+import com.sun.tools.javac.util.StringUtils;
 import com.sun.xml.internal.bind.v2.runtime.output.Encoded;
 
 import java.io.UnsupportedEncodingException;
@@ -8,6 +9,7 @@ import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Date;
 
 /**
  * @Author rookie.li
@@ -15,20 +17,32 @@ import java.security.NoSuchAlgorithmException;
  */
 public class EncodeDemo {
     public static void main(String[] args) {
-        try {
-            String liwenjie = URLEncoder.encode("奋斗", "UTF-8");
-            getMD5Str("12345");
-            System.out.println("liwenjie = " + liwenjie);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+        String liwenjie = encode("奋斗");
+        String pwd = "AMP001" + "00000000" + "123456" + System.currentTimeMillis();
+        System.out.println("pwd = " + pwd);
+        String j10003000000001111110803192020 = getMD5Str(pwd);
+        System.out.println("liwenjie = " + liwenjie);
+        System.out.println("liwenjie = " + j10003000000001111110803192020);
     }
 
-        public static String getMD5Str(String str) {
+    public static String encode(String str) {
+        if (str != null && str.length() > 0) {
+            try {
+                String encodeStr = URLEncoder.encode(str, "UTF-8");
+                return encodeStr;
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    public static String getMD5Str(String str) {
+        if (str != null && str.length() > 0) {
             byte[] digest = null;
             try {
                 MessageDigest md5 = MessageDigest.getInstance("md5");
-                digest  = md5.digest(str.getBytes("utf-8"));
+                digest = md5.digest(str.getBytes("utf-8"));
             } catch (NoSuchAlgorithmException e) {
                 e.printStackTrace();
             } catch (UnsupportedEncodingException e) {
@@ -38,5 +52,7 @@ public class EncodeDemo {
             String md5Str = new BigInteger(1, digest).toString(16);
             return md5Str;
         }
+        return null;
+    }
 
 }
