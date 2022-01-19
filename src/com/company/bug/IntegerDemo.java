387b65c3167fbd5689990e21dee90a070f08599f
rookieliiwenjie <1154506107@qq.com>
2022-01-19 19:31:55 +0800

    feat：验证代码

---
+++ src/com/company/bug/IntegerDemo.java
@@ -0,0 +1,8 @@
+package com.company.bug;
+
+public class IntegerDemo{
+    public static void main(String[] args) {
+        Integer i = null;
+        System.out.println(        i.intValue()        );
+    }
+}
