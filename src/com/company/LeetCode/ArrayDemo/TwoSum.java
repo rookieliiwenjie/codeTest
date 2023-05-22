027862dac01e57dac8a57a98d6eb4245c8c8032e
rookieliiwenjie <1154506107@qq.com>
2023-05-22 23:58:42 +0800

    feat: 层序遍历，

--- src/com/company/LeetCode/ArrayDemo/TwoSum.java
+++ src/com/company/LeetCode/ArrayDemo/TwoSum.java
@@ -14,8 +14,11 @@ import java.util.HashMap;
 public class TwoSum {
     public static void main(String[] args) {
         int nums[] = {2, 7, 11, 14};
-        twoSum(nums, 25);
-        System.out.println("nums = " + nums);
+
+//        System.out.println("nums = " + twoSum(nums, 25));
+//        System.out.println("nums2 = " + twoSum2(nums, 25));
+        System.out.println("nums2 = " + onStartup(nums, 25));
+
     }
 
     public static int[] twoSum(int[] nums, int target) {
@@ -35,6 +38,67 @@ public class TwoSum {
         newnums[0] = nums[i];
         newnums[1] = nums[j];
         return newnums;
+    }
+
+
+    public static int[] twoSum2(int[] nums, int target) {
+        int newNums[] = new int[2];
+        int i = 0;
+        int j = 1;
+        while (i < nums.length || j < nums.length) {
+            if(j==nums.length){
+                i++;
+                j=i+1;
+            }
+            if ( (nums[i] + nums[j]) < target) {
+                j++;
+            } else if ((nums[i] + nums[j]) > target) {
+                i++;
+            } else {
+                newNums[0] = i;
+                newNums[1] = j;
+                break;
+            }
+        }
+        return newNums;
+    }
+
+    /**
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum3(int[] nums, int target) {
+        int newNums[] = new int[2];
+        int left = 0;
+        int right = nums.length-1;
+        while (left<=right){
+            int mid = left + (right-left)/2;
+//            if(){
+//
+//            }
+
+        }
+        return newNums;
+    }
+
+    public static int[] onStartup(int[] nums, int target) {
+        int newNums[] = new int[2];
+        int left = 0;
+        int right = nums.length-1;
+        while (left<=right){
+            if(nums[left]+nums[right]<target){
+                left++;
+            }else if(nums[left]+nums[right]>target){
+                right--;
+            }else {
+                newNums[0] = left;
+                newNums[1] = right;
+                break;
+            }
+
+        }
+        return newNums;
     }
 
 //    public static int[] twoSumTwo(int[] nums, int target) {
