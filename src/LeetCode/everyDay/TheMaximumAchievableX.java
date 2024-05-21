33f45bc9d3bd201731b21e56e64fcbf9ccbb9a09
rookieliiwenjie <1154506107@qq.com>
2024-05-21 14:35:57 +0800

    小脑萎缩题

---
+++ src/LeetCode/everyDay/TheMaximumAchievableX.java
@@ -0,0 +1,10 @@
+package LeetCode.everyDay;
+
+/**
+ * 小脑萎缩题
+ */
+public class TheMaximumAchievableX {
+    public int theMaximumAchievableX(int num, int t) {
+        return num + 2 * t;
+    }
+}
