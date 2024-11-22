a5cdeabce875459a4d2f56fb6c858e53fd91f396
rookieliiwenjie <1154506107@qq.com>
2024-11-22 23:41:39 +0800

    图片模糊器

--- src/LeetCode/everyDay/ImageSmoother.java
+++ src/LeetCode/everyDay/ImageSmoother.java
@@ -4,9 +4,10 @@ public class ImageSmoother {
 
     public static void main(String[] args) {
         ImageSmoother imageSmoother = new ImageSmoother();
-//        int[][] img = { { 1, 1, 1 }, { 1, 0, 1 }, { 1, 1, 1 } };
-//        int[][] img = { { 2, 3, 4 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 }, { 14, 15, 16 } };
-        int[][] img = {{100, 200, 100}, {200, 50, 200}, {100, 200, 100}};
+        // int[][] img = { { 1, 1, 1 }, { 1, 0, 1 }, { 1, 1, 1 } };
+        // int[][] img = { { 2, 3, 4 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 }, { 14,
+        // 15, 16 } };
+        int[][] img = { { 100, 200, 100 }, { 200, 50, 200 }, { 100, 200, 100 } };
         int[][] ints = imageSmoother.ImageSmoother(img);
         for (int[] anInt : ints) {
             for (int i : anInt) {
@@ -18,9 +19,9 @@ public class ImageSmoother {
     }
 
     public int[][] ImageSmoother(int img[][]) {
-        //0=x 1=y
+        // 0=x 1=y
         // 左，左上，上 ， 右上 ， 右 ， 右下，下，左下
-        int[][] index = {{-1, 0}, {-1, -1}, {0, -1}, {1, -1}, {0, 1}, {1, 1}, {1, 0}, {-1, 1}};
+        int[][] index = { { -1, 0 }, { -1, -1 }, { 0, -1 }, { 1, -1 }, { 0, 1 }, { 1, 1 }, { 1, 0 }, { -1, 1 } };
         int m = img.length;
         int n = img[0].length;
         int[][] arr = new int[m][n];
